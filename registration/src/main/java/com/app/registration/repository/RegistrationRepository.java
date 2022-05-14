3ac8a4c1be41aa428272ce2a27392948fce1427a
mindsdetjava42 <mindsdetjava42@mindc1j149.makemylabs.in>
2022-05-14 11:34:55 +0530

    register commit

--- registration/src/main/java/com/app/registration/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.registration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.registration.model.User;
-
-public interface RegistrationRepository extends JpaRepository<User, Integer> {
-
-}
